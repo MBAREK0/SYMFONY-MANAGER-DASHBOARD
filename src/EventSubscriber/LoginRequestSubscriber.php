341ac3b603ed6c5fa3e1fe901ec1b2cce894caa5
MBAREK0 <elaadraouimbarek2023@gmail.com>
2024-06-28 16:07:36 +0100

    ✨ finiched the portfolio

--- src/EventSubscriber/LoginRequestSubscriber.php
+++ src/EventSubscriber/LoginRequestSubscriber.php
@@ -19,16 +19,6 @@ class LoginRequestSubscriber implements EventSubscriberInterface
 
     public function onKernelRequest(RequestEvent $event)
     {
-        $request = $event->getRequest();
-        // $request->request->set('_username', 'fh@gmail.com');
-        // dd($request->request->get('_username'));
-        // if($request->isMethod('POST'))
-        // if($request->request->get('_username') !== null)
-        // dd($request->request->get('_username'));
-
-        // Check if the request is for the login route and is a POST request
-        if ($request->attributes->get('_route') === 'app_auth') {
-            // Modify the _username parameter
-        }
+        $request = $event->getRequest();      
     }
 }
